8e63c58a16ce13c382580954a9fd061c5d59dac4
mahendaraengala <mahendarmahi1998@gmail.com>
2024-06-03 09:07:43 +0530

    Add files via upload

---
+++ about-hari-reddy-andluru.php
@@ -0,0 +1,46 @@
+<?php include('header.php') ?>
+<div class="stricky-header stricked-menu main-menu">
+    <div class="sticky-header__content"></div><!-- /.sticky-header__content -->
+</div><!-- /.stricky-header -->
+<section class="about_team_members_page pad_50">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-5 col-lg-5 wow fadeInUp animated" data-wow-delay="100ms" style="visibility: visible; animation-delay: 100ms; animation-name: fadeInUp;">
+                <!--Team One Single-->
+                <div class="team-one__single">
+                    <div class="team-one__img">
+                        <img src="assets/img/team/Harinath Reddy-.jpg" alt="" loading="lazy">
+                        <ul class="list-unstyled team-one__social">
+                            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+                        </ul>
+                    </div>
+                    <div class="team-one__content-box">
+                        <div class="team-one__content">
+                            <div class="team-one-shape">
+                                <img src="assets/images/shapes/team-one-shape.png" alt="" loading="lazy">
+                            </div>
+                            <h4 class="team-one__name"><a>Hari Reddy Andluru  </a></h4>
+                            <p class="team-one__title">(Head of SAP Competency)
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-7 person_discription ">
+                <h3>Professional Overview</h3>
+<p>Hari spearheads SAP Competency to make sure the resources are well trained and delivery ready, in the in-demand most anticipated technologies. He believes human resources are crucial assets in information technology world and Resources with right skillset are key to the success any technology initiative.</p>
+
+<p>Having 14 years for distinguished experience in IT, worked with clients from multiple geographic locations spanning from North America, Europe, to MENA and Southeast Asia, in multiple industry verticals ranging from retail, Elections manufacturing and Specialty chemicals to name a few.</p>
+
+<p>With the knowledge of working in multiple Customer Delivery roles, gained profound knowledge of SAP ABAP, MM and EWM Module. He is SAP certified Consultant. </p>
+
+<p>He has Post Graduate Degree in Master of Computer Applications and Graduate Degree of Bachelor of Science in Mathematics. He is a financial market enthusiast and loves to watch cinemas & cricket.</p>
+
+
+
+            </div>
+        </div>
+    </div>
+</section>
+<?php include('footer.php') ?>
